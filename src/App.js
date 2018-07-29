2d8cca7898b44d38d29dad8242e3fff00589eeee
Werberth Vinícius <werberthvinicius@gmail.com>
2018-07-28 23:56:50 -0300

    Add ListContacs component as class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import ListContacs from './ListContacts';
+
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+]
 
 
 class App extends Component {
   render(){
-    return <div>Hello World</div>
+    return (
+      <div>
+        <ListContacs contacts={contacts}/>
+      </div>
+    )
   }
 }
 
