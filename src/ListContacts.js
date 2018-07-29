3e9a88f078d44fb433087fc9aff60a42a42b47c4
Werberth Vinícius <werberthvinicius@gmail.com>
2018-07-29 03:04:55 -0300

    Making the List Contacts component less cumbersome

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -19,12 +19,15 @@ class ListContacts extends Component {
     }
 
     render(){
+        const { contacts, onDeleteContact } = this.props;
+        const { query } = this.state;
+
         let showingContacts;
-        if (this.state.query) {
+        if (query) {
             const match = new RegExp(escapeRegExp(this.state.query), 'i')
-            showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+            showingContacts = contacts.filter((contact) => match.test(contact.name))
         } else {
-            showingContacts = this.props.contacts;
+            showingContacts = contacts;
         }
 
         showingContacts.sort(sortBy('name'));
@@ -37,7 +40,7 @@ class ListContacts extends Component {
                         className='search-contacts'
                         type='text'
                         placeholder='Search contacts'
-                        value={this.state.query}
+                        value={query}
                         onChange={(event) => this.updateQuery(event.target.value)}
                     />
                 </div>
@@ -54,7 +57,7 @@ class ListContacts extends Component {
                             </div>
                             <button 
                                 className="contact-remove"
-                                onClick={() => this.props.onDeleteContact(contact)}
+                                onClick={() => onDeleteContact(contact)}
                             >
                                 Remove
                             </button>
