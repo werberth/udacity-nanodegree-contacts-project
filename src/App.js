90f229738745119baa87bdaf4c4777053e7cf5c4
Werberth Vinícius <werberthvinicius@gmail.com>
2018-07-30 02:07:02 -0300

    Calling ContactsAPI.remove function in removeContact method of App component

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ class App extends Component {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
     }))
+
+    ContactsAPI.remove(contact);
   }
  
   render(){
